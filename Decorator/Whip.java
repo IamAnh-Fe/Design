b19360d3529fa00cbf7209224c65b99ddbc91bba
Pham Tien Anh <phamtienanhdev@gmail.com>
2023-09-21 23:19:43 +0700

    Factory

--- Decorator/Whip.java
+++ Decorator/Whip.java
@@ -1,16 +1,16 @@
-// package Decorator;
+package Decorator;
 
-// public class Whip extends CondimentDecorator {
-//   Beverage beverage;
-// 	public Whip(Beverage beverage) {
-// 		this.beverage = beverage;
-// 	}
+public class Whip extends CondimentDecorator {
+  Beverage beverage;
+	public Whip(Beverage beverage) {
+		this.beverage = beverage;
+	}
  
-// 	public String getDescription() {
-// 		return beverage.getDescription() + ", Whip";
-// 	}
+	public String getDescription() {
+		return beverage.getDescription() + ", Whip";
+	}
  
-// 	public double cost() {
-// 		return .10 + beverage.cost();
-// 	}
-// }
+	public double cost() {
+		return .10 + beverage.cost();
+	}
+}
